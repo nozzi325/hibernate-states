938453ac588a117fcd13dd8674f171ce6ddb6c8f
nozzi325 <nozzia3@gmail.com>
2023-10-14 09:30:10 +0300

    Updated code formatting

--- src/main/java/com/example/hibernatestates/service/ContactService.java
+++ src/main/java/com/example/hibernatestates/service/ContactService.java
@@ -6,19 +6,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ContactService {
-    public void persistContact(Contact contact, Session session){
+    public void persistContact(Contact contact, Session session) {
         session.persist(contact);
     }
 
-    public void updateContact(Contact contact, Session session){
-        if(session.find(Contact.class, contact.getId()) == null){
+    public void updateContact(Contact contact, Session session) {
+        if (session.find(Contact.class, contact.getId()) == null) {
             throw new RuntimeException("Contact with id " + contact.getId() + " not found");
         }
         session.merge(contact);
     }
 
-    public void deleteContact(Contact contact, Session session){
-        if(session.find(Contact.class, contact.getId()) == null){
+    public void deleteContact(Contact contact, Session session) {
+        if (session.find(Contact.class, contact.getId()) == null) {
             throw new RuntimeException("Contact with id " + contact.getId() + " not found");
         }
         session.remove(contact);
--- src/main/java/com/example/hibernatestates/service/ProjectService.java
+++ src/main/java/com/example/hibernatestates/service/ProjectService.java
@@ -1,37 +1,34 @@
 package com.example.hibernatestates.service;
 
-
 import com.example.hibernatestates.entity.Project;
 import org.hibernate.Session;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class ProjectService {
-    public void persistProject(Project project, Session session){
+    public void persistProject(Project project, Session session) {
         session.persist(project);
     }
 
-    public void saveProject(Project project, Session session){
+    public void saveProject(Project project, Session session) {
         session.save(project);
     }
 
-    public void updateProject(Project project, Session session){
-        if (this.getProjectById(project.getId(), session) != null){
+    public void updateProject(Project project, Session session) {
+        if (getProjectById(project.getId(), session) != null) {
             session.merge(project);
         }
     }
 
-    public void deleteProject(Project project, Session session){
-        if (this.getProjectById(project.getId(), session) != null){
+    public void deleteProject(Project project, Session session) {
+        if (getProjectById(project.getId(), session) != null) {
             session.remove(project);
         }
     }
 
-    public Project getProjectById(Integer id, Session session){
+    public Project getProjectById(Integer id, Session session) {
         Project project = session.find(Project.class, id);
-        if (project == null){
+        if (project == null) {
             throw new RuntimeException("Project with id " + id + " not found");
         }
         return project;
